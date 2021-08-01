92056da18b08b851ebac0908d344efa1eb775305
Lev-coder <54110434+Lev-coder@users.noreply.github.com>
2021-08-01 18:14:28 +0300

    add products and user controllers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use \App\Http\Controllers\Restorun\ProductController;
+use \App\Http\Controllers\Restorun\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('/product',ProductController::class);
+Route::resource('/user',UserController::class);
+
